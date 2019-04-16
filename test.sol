3c9872d9d0a6633f60e3f111c984c7296dfec757
kirkb <insomz@gmail.com>
2019-04-16 18:33:07 -0500

    test

---
+++ test.sol
@@ -0,0 +1,9 @@
+contract Tokensale {
+    uint hardcap = 10000 ether;
+    function fetchCap() public returns(uint) {
+        return hardcap;
+    }
+}
+contract Presale is Tokensale {
+    uint hardcap = 1000 ether;
+}
